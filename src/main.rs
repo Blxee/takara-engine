015e0f261d89e715f82bd0013b8a65684ed806eb
Blxee <aminedev57@gmail.com>
2025-06-21 18:13:14 +0100

    Added: templates and structs outline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
     println!("Hello, world!");
 }
+
+mod tak_board {
+    struct TakBoard<const N: usize = 5> {
+        size: i32,
+        grid: [[Cell<N>; N]; N],
+    }
+
+    struct Cell<const N: usize = 5> {
+        stack: [Piece; N],
+    }
+
+    struct Piece {
+        color: PieceColor,
+        typ: PieceType,
+    }
+
+    enum PieceColor {
+        White,
+        Black,
+    }
+
+    enum PieceType {
+        FlatStone,
+        StandingStone,
+        CapStone,
+    }
+
+    impl TakBoard {
+        const fn new() -> Self {
+            Self {
+                size: todo!(),
+                grid: todo!(),
+            }
+        }
+    }
+}
