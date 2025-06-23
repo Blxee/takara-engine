b6453ae3a40bde1a10073f859bd6dd532e9ec093
Blxee <aminedev57@gmail.com>
2025-06-23 16:10:29 +0100

    Changed: organization of the project and added clap args

---
+++ src/cli.rs
@@ -0,0 +1,7 @@
+use crate::tak_board;
+
+pub fn start_game() {
+    let board = tak_board::TakBoard::new();
+    println!("{board}");
+    // TODO: implement the game loop
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+mod cli;
 mod tak_board;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    #[arg(short, long, value_name = "PORT")]
+    listen: Option<i32>,
+}
 
 fn main() {
-    println!("Hello, world!");
-    let mut board = tak_board::TakBoard::new();
-    println!("{board}");
+    let args = Args::parse();
+    match args.listen {
+        None => cli::start_game(),
+        Some(port) => todo!("make the server to listen on"),
+    }
 }
