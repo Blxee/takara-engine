b406cfbd4c6a8e37a651486645a0ab0c0a36e034
Blxee <aminedev57@gmail.com>
2025-06-27 13:49:24 +0100

    Added: TryFrom<&str> to the Position struct to use format like "a4"

--- src/types.rs
+++ src/types.rs
@@ -49,6 +49,33 @@ impl SubAssign for Position {
     }
 }
 
+impl TryFrom<&str> for Position {
+    type Error = &'static str;
+
+    /// Convert a position string to a Position struct
+    /// 
+    /// # Arguments:
+    ///
+    /// * `value` - a string which contains a digit for the row
+    /// and a letter for the column
+    /// ---
+    /// > Note:
+    /// any characters which do not represent either row or col will be ignored.
+    /// ony the first row and first col matched will be used.
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        const VALID_ROWS: &str = "12345678";
+        const VALID_COLS: &str = "abcdefgh";
+        let value = value.to_lowercase();
+        let Some(row) = value.chars().find_map(|c| VALID_ROWS.find(c)) else {
+            return Err("no valid row was found");
+        };
+        let Some(col) = value.chars().find_map(|c| VALID_COLS.find(c)) else {
+            return Err("no valid column was found");
+        };
+        Ok(Position::new(row as i32, col as i32))
+    }
+}
+
 #[derive(Clone, Copy)]
 pub enum StoneType {
     FlatStone,
